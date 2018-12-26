5a4d14649ae254e49676b50ead86ea9ca31ed4a3
Pal <anirban.pal@mastercard.com>
2018-12-26 12:17:16 -0600

    Changed location of application.properties

---
+++ Git Init a New Repo.sh
@@ -0,0 +1,7 @@
+echo "# Spring Cloud ConfigServer" >> README.md
+git init
+git add README.md
+git commit -am "First commit to init Repo with only README.md"
+git remote add origin https://github.com/AnirbanPals/configServer.git
+# git remote set-url origin https://github.com/AnirbanPals/configServer.git 
+git push -u origin master
